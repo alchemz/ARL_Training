3c6d36119267a53a51864ad603ad266e9df54f8f
xianling zhang <alchemxz@gmail.com>
2017-02-12 21:09:44 -0500

    Add files via upload

---
+++ Trump/Assets/DV/Trump/changeFace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeFace : MonoBehaviour
+{
+    public Texture[] textures;
+    public float changeInterval = 0.33F;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (textures.Length == 0)
+            return;
+  
+        int index = Mathf.FloorToInt(Time.time / changeInterval);
+        index = index % textures.Length;
+        if(Input.GetMouseButtonDown(0))
+        rend.material.mainTexture = textures[index];
+    }
+}
